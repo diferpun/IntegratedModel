a434c42dfb2a8dd47487853814d36c246a05f3cf
diego <diferpun@gmail.com>
2022-09-26 21:58:33 -0500

    hyper_SVD_27_final

--- mainHyper.py
+++ mainHyper.py
@@ -16,7 +16,7 @@ if __name__ == '__main__':
    ########## important paths ###################################################################
    #hyper_grid_random = randomSearch(20)
 
-   hyper_grid_random=[(Adam,0.001,2,1e-05)]
+   hyper_grid_random=[(Nadam, 0.0001, 1, 0.0001)]
    dataDir    = "/home/andres_david_0496/dataSets"
    modelsDir  = "/home/andres_david_0496"
 
@@ -24,12 +24,12 @@ if __name__ == '__main__':
    #modelsDir = "."
 
    Lmax=430
-   dr = "PCA"
+   dr = "SVD"
    rawflag=False
    isnorm=False
    ds=["Train","Valid","Test"]
-   dim  = 24
-   srd  = 997
+   dim  = 27
+   srd  = 1461
    epch = 50 ################################## importante #########################################
 
    if dr=="RAW":
