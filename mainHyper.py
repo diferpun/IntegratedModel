7f2c2179b5774ceb2cc478f187c7e2e0910d3993
diego <diferpun@gmail.com>
2022-10-02 08:01:57 -0500

    hyper_AE_24_final

--- mainHyper.py
+++ mainHyper.py
@@ -16,7 +16,7 @@ if __name__ == '__main__':
    ########## important paths ###################################################################
    #hyper_grid_random = randomSearch(20)
 
-   hyper_grid_random=[(Adamax,0.0001, 2,0.001)]
+   hyper_grid_random=[(Adam, 0.001,	1,1e-05)]
    dataDir    = "/home/andres_david_0496/dataSets"
    modelsDir  = "/home/andres_david_0496"
 
@@ -24,12 +24,12 @@ if __name__ == '__main__':
    #modelsDir = "."
 
    Lmax=430
-   dr = "AE"
+   dr = "RAW"
    rawflag=False
    isnorm=False
    ds=["Train","Valid","Test"]
    dim  = 24
-   srd  = 506
+   srd  = 808
    epch = 50 ################################## importante #########################################
 
    if dr=="RAW":
